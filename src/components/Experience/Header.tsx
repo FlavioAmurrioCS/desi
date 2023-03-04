2d58ab75f200d268606bd37b42e1ba11bb7460fe
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2023-03-04 10:07:42 -0500

    Updated introduction

--- src/components/Experience/Header.tsx
+++ src/components/Experience/Header.tsx
@@ -5,7 +5,9 @@ export default function Header() {
   return (
     <div className="header-container">
       <h1>Past Work Projects</h1>
-      <p className="subtitle">{constants.OUR_PARTNERS}</p>
+      <p className="subtitle line-break project-container">
+        {constants.OUR_PARTNERS}
+      </p>
       <div className="pretend-footer">
         <p>
           Learn more about <Link to="/services">Our Services</Link>{" "}
